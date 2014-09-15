5ad9d8eb62b79557ec032662b368e867c50a2e50
Xinyue Zhao <czhao@cellular.de>
2014-09-15 22:53:13 +0200

    Added menu for searchview, but still problem.

--- app/src/main/java/com/itbooks/app/MainActivity.java
+++ app/src/main/java/com/itbooks/app/MainActivity.java
@@ -1,28 +1,44 @@
 package com.itbooks.app;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
+import android.support.v7.widget.SearchView.OnSuggestionListener;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
 
 import com.android.volley.Request.Method;
 import com.chopping.net.GsonRequestTask;
-import com.chopping.utils.Utils;
 import com.itbooks.R;
 import com.itbooks.adapters.BookListAdapter;
 import com.itbooks.data.DSBookList;
 import com.itbooks.utils.Prefs;
 
 
-public class MainActivity extends BaseActivity implements OnRefreshListener {
+public class MainActivity extends BaseActivity implements OnRefreshListener, OnQueryTextListener {
 	/**
 	 * Main layout for this component.
 	 */
 	private static final int LAYOUT = R.layout.activity_main;
-
+	private static final int MAIN_MENU = R.menu.main_menu;
 	private SwipeRefreshLayout mRefreshLayout;
 	private ListView mLv;
 	private BookListAdapter mAdp;
+	private View mInitLl;
+	private SearchRecentSuggestions mSuggestions;
+	private String mKeyword;
+	private SearchView mSearchView;
 
 	//------------------------------------------------
 	//Subscribes, event-handlers
@@ -35,7 +51,6 @@ public class MainActivity extends BaseActivity implements OnRefreshListener {
 	 * 		Event {@link com.itbooks.data.DSBookList}.
 	 */
 	public void onEvent(DSBookList e) {
-		Utils.showShortToast(this, "get books: " + e.getBooks().size());
 		mRefreshLayout.setRefreshing(false);
 
 		if (mAdp == null) {
@@ -45,6 +60,8 @@ public class MainActivity extends BaseActivity implements OnRefreshListener {
 			mAdp.setData(e.getBooks());
 			mAdp.notifyDataSetChanged();
 		}
+
+		dismissInitView();
 	}
 
 
@@ -54,17 +71,92 @@ public class MainActivity extends BaseActivity implements OnRefreshListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(LAYOUT);
+		mSuggestions = new SearchRecentSuggestions(this, SearchSuggestionProvider.AUTHORITY,
+				SearchSuggestionProvider.MODE);
+
 		mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.content_srl);
 		mRefreshLayout.setColorSchemeResources(R.color.green_1, R.color.green_2, R.color.green_3, R.color.green_4);
 		mRefreshLayout.setOnRefreshListener(this);
 		mRefreshLayout.setRefreshing(true);
-
 		mLv = (ListView) findViewById(R.id.books_lv);
+		mInitLl = findViewById(R.id.init_ll);
+
+		handleIntent(getIntent());
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(MAIN_MENU, menu);
+		final MenuItem searchMenu = menu.findItem(R.id.search);
+		mSearchView = (SearchView) MenuItemCompat.getActionView(searchMenu);
+		mSearchView.setOnQueryTextListener(this);
+		/* In order to close ActionView automatically after clicking keyboard. */
+		mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+
+			@Override
+			public void onFocusChange(View _v, boolean _hasFocus) {
+				if (!_hasFocus) {
+					MenuItemCompat.collapseActionView(searchMenu);
+					mSearchView.setQuery("", false);
+				}
+			}
+		});
+		mSearchView.setOnSuggestionListener(new OnSuggestionListener() {
+			@Override
+			public boolean onSuggestionSelect(int _pos) {
+				return false;
+			}
+
+			@Override
+			public boolean onSuggestionClick(int _pos) {
+				MenuItemCompat.collapseActionView(searchMenu);
+				mSearchView.setQuery("", false);
+				return false;
+			}
+		});
+		SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+		if (searchManager != null) {
+			SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
+			mSearchView.setSearchableInfo(info);
+		}
+		return true;
+	}
+
+	protected void handleIntent(Intent _intent) {
+		mRefreshLayout.setRefreshing(true);
+		mKeyword = _intent.getStringExtra(SearchManager.QUERY);
+		if (!TextUtils.isEmpty(mKeyword)) {
+			mKeyword = mKeyword.trim();
+			resetSearchView();
+			mSuggestions.saveRecentQuery(mKeyword, null);
+		}
+	}
+
+
+	/**
+	 * Reset the UI status of searchview.
+	 */
+	protected void resetSearchView() {
+		if (mSearchView != null) {
+			mSearchView.clearFocus();
+		}
+	}
+
+	@Override
+	protected void onNewIntent(Intent _intent) {
+		super.onNewIntent(_intent);
+		setIntent(_intent);
+		handleIntent(_intent);
 	}
 
 	@Override
 	public void onRefresh() {
-		loadDefaultPage();
+		if (!TextUtils.isEmpty(mKeyword)) {
+			loadByKeyword();
+		} else {
+			loadDefaultPage();
+		}
 	}
 
 	/**
@@ -75,9 +167,41 @@ public class MainActivity extends BaseActivity implements OnRefreshListener {
 		new GsonRequestTask<DSBookList>(getApplicationContext(), Method.GET, url, DSBookList.class).execute();
 	}
 
+
+	/**
+	 * Page when nothing wanna be searched.
+	 */
+	private void loadByKeyword() {
+		String url = Prefs.getInstance(getApplication()).getApiSearchBooks();
+		url = String.format(url, mKeyword, "1");
+		new GsonRequestTask<DSBookList>(getApplicationContext(), Method.GET, url, DSBookList.class).execute();
+	}
+
 	@Override
 	protected void onAppConfigLoaded() {
 		super.onAppConfigLoaded();
 		loadDefaultPage();
+	}
+
+	private void dismissInitView() {
+		mInitLl.setVisibility(View.GONE);
+	}
+
+	private void showInitView() {
+		mInitLl.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public boolean onQueryTextSubmit(String s) {
+		InputMethodManager mgr = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+		mgr.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0);
+		resetSearchView();
+		return false;
+	}
+
+
+	@Override
+	public boolean onQueryTextChange(String s) {
+		return false;
 	}
 }
---
+++ app/src/main/java/com/itbooks/app/SearchSuggestionProvider.java
@@ -0,0 +1,17 @@
+package com.itbooks.app;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+/**
+ * Database for all searched suggestions.
+ *
+ * @author Xinyue Zhao
+ */
+public final class SearchSuggestionProvider extends SearchRecentSuggestionsProvider {
+	public final static String AUTHORITY = "com.itbooks";
+	public final static int MODE = DATABASE_MODE_QUERIES;
+
+	public SearchSuggestionProvider() {
+		setupSuggestions(AUTHORITY, MODE);
+	}
+}
