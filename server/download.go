3c4600b846cd3170821851c01459d099f59ec96b
XinyueZhao <zhao@eosnewmedia.de>
2015-04-28 22:54:49 +0200

    API port switched to post with json-body.

--- server/download.go
+++ server/download.go
@@ -3,12 +3,14 @@ package itbooks
 import (
 	"fmt"
 	"net/http"
+_	"net/url"
   "io/ioutil"
   "appengine"
   "appengine/urlfetch"
 	"bytes"
 	"encoding/xml"
 	"encoding/json"
+	_ "strings"
 )
 
 type Error string
@@ -19,6 +21,10 @@ func (e Error) Error() string {
 
 func init() {
 	http.HandleFunc("/download", handleDownload)
+}
+
+type SearchBody struct {
+		Query string `xml:"query"`
 }
 
 func handleDownload(w http.ResponseWriter, r *http.Request) {
@@ -34,21 +40,26 @@ func handleDownload(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	//Get book fullname.
-	args := r.URL.Query()
-	name := args["n"][0]
-
-  ch := make(chan []Book)
-  go download(cxt, name, ch)
-  list := <-ch
-	if list != nil {
-		jstr, _ := json.Marshal(&list)
-		s := fmt.Sprintf(`{"status":%d, "result":%s}`, 200, jstr)
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, s)
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, `{"status":%d}`, 300)
+	pSearchBody := new(SearchBody)
+	if bytes, e := ioutil.ReadAll(r.Body); e == nil {
+		if e := json.Unmarshal(bytes, pSearchBody); e == nil {
+			ch := make(chan []*Book)
+		  go download(cxt, pSearchBody.Query, ch)
+		  list := <-ch
+			if list != nil {
+				jstr, _ := json.Marshal(&list)
+				s := fmt.Sprintf(`{"status":%d, "keyword" : "%s", "count" : %d, "result":%s}`, 200, pSearchBody.Query, len(list), jstr)
+				w.Header().Set("Content-Type", "application/json")
+				fmt.Fprintf(w, s)
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				fmt.Fprintf(w, `{"status":%d}`, 300)
+			}
+			return
+		}
 	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status":%d}`, 300)
 }
 
 const SOAP =
@@ -90,16 +101,32 @@ type NewDataSet struct {
 }
 
 type Book struct {
+	Name string  `xml:"_name2"`
+	Author string  `xml:"_name3"`
 	Size string  `xml:"_size"`
+	Pages string `xml:"_pages"`
 	Link  string `xml:"_download_link"`
+	ISBN string  `xml:"_ISBN"`
+	Year string  `xml:"_year"`
+	Publisher string  `xml:"_name"`
+	CoverUrl string  `xml:"_book_img_url"`
+	Description string `xml:"_desc"`
+}
+
+type TinyUrl struct {
+	Status bool `json:"status"`
+	Result string  `json:"result"`
 }
 
 
-func download(cxt appengine.Context, keyword string, ch chan []Book) {
+func download(cxt appengine.Context, keyword string, ch chan []*Book) {
 	client := urlfetch.Client(cxt)
-  body := fmt.Sprintf(SOAP, keyword)
-	if r, e := http.NewRequest("POST", API, bytes.NewBufferString(body)); e == nil {
 
+	cxt.Infof("keyword: %v", keyword)
+	body := fmt.Sprintf(SOAP, keyword)
+
+	if r, e := http.NewRequest("POST", API, bytes.NewBufferString(body)); e == nil {
+		cxt.Infof("body: %v", body)
 		r.Header.Add("User-Agent", "ksoap2-android/2.6.0+")
 		r.Header.Add("SOAPAction", "http://tempuri.org/SearchBooks")
 		r.Header.Add("Content-Type", "text/xml;charset=utf-8")
@@ -115,7 +142,71 @@ func download(cxt appengine.Context, keyword string, ch chan []Book) {
 			if bytes, e := ioutil.ReadAll(resp.Body); e == nil {
 				pSoapResult := new(Envelope)
 				if e := xml.Unmarshal(bytes, pSoapResult); e == nil {
-          ch <- pSoapResult.Body.SearchBooksResponse.SearchBooksResult.Diffgram.NewDataSet.BookList[:]
+					books := pSoapResult.Body.SearchBooksResponse.SearchBooksResult.Diffgram.NewDataSet.BookList[:]
+					cxt.Infof("books: %v", books)
+
+					//Make tinyurl to download-links.
+					if books != nil {
+						sz := len(books)
+						chh := make(chan *Book,  sz)
+						for _, b := range books {
+							go func( book Book, ch chan *Book) {
+									ret  := Book{ 	book.Name,
+										book.Author,
+										book.Size,
+										book.Pages,
+										book.Link,
+										book.ISBN,
+										book.Year,
+										book.Publisher,
+										book.CoverUrl,
+										book.Description }
+									tinyQ := fmt.Sprintf("%s%s", "https://tinyurl-wrapper.appspot.com/?q=", ret.Link)
+									if r, e := http.NewRequest("GET", tinyQ, nil); e == nil {
+										if resp, e := client.Do(r); e == nil {
+											if resp != nil {
+												defer resp.Body.Close()
+											}
+											if bytes, e := ioutil.ReadAll(resp.Body); e == nil {
+													pTiny := new(TinyUrl)
+													if e := json.Unmarshal(bytes, pTiny); e == nil {
+														if pTiny.Status   && pTiny.Result != "" {
+															ret.Link = pTiny.Result
+														}
+													}
+											}
+										}
+									}
+
+									tinyQ = fmt.Sprintf("%s%s", "https://tinyurl-wrapper.appspot.com/?q=", ret.CoverUrl)
+									if r, e := http.NewRequest("GET", tinyQ, nil); e == nil {
+										if resp, e := client.Do(r); e == nil {
+											if resp != nil {
+												defer resp.Body.Close()
+											}
+											if bytes, e := ioutil.ReadAll(resp.Body); e == nil {
+													pTiny := new(TinyUrl)
+													if e := json.Unmarshal(bytes, pTiny); e == nil {
+														if pTiny.Status   && pTiny.Result != "" {
+															ret.CoverUrl = pTiny.Result
+														}
+													}
+											}
+										}
+									}
+									ch <- &ret
+							}(b, chh)
+						}
+
+						booksRet := make([]*Book, sz)
+						for i := 0; i < sz; i++ {
+								pBook := <-chh
+								booksRet[i] = pBook
+						}
+						ch <- booksRet[:]
+					} else {
+						ch <- nil
+					}
 				} else {
           ch <- nil
 					cxt.Errorf("Error but still going: %v", e)
