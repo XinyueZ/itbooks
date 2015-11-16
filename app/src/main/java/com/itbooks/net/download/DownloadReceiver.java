054cfb1724a364b62e121d3ea2443b93a9808ad7
XinyueZhao <zhao@eosnewmedia.de>
2015-11-16 15:25:50 +0100

    Refactor notificaiton of download

--- app/src/main/java/com/itbooks/net/download/DownloadReceiver.java
+++ app/src/main/java/com/itbooks/net/download/DownloadReceiver.java
@@ -1,6 +1,7 @@
 package com.itbooks.net.download;
 
 import java.io.File;
+import java.io.IOException;
 
 import android.app.DownloadManager;
 import android.app.NotificationManager;
@@ -9,15 +10,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.BigPictureStyle;
 import android.support.v4.app.NotificationCompat.BigTextStyle;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.os.AsyncTaskCompat;
 
+import com.chopping.utils.Utils;
 import com.itbooks.R;
 import com.itbooks.app.App;
 import com.itbooks.bus.DownloadCompleteEvent;
 import com.itbooks.db.DB;
+import com.squareup.picasso.Picasso;
 
 import de.greenrobot.event.EventBus;
 
@@ -44,14 +54,43 @@ public final class DownloadReceiver extends BroadcastReceiver {
 				switch (status) {
 				case DownloadManager.STATUS_SUCCESSFUL:
 					download.end(context);
+					AsyncTaskCompat.executeParallel(new AsyncTask<Download, Void, Bitmap>() {
+						private Download mDownload;
+						@Override
+						protected Bitmap doInBackground(Download... params) {
+							mDownload = params[0];
+							Picasso picasso = Picasso.with(App.Instance);
+							try {
+								return picasso.load(Utils.uriStr2URI(mDownload.getCoverUrl()).toASCIIString()).get();
+							} catch (IOException e) {
+								return null;
+							}
+						}
 
-					File to = new File(App.Instance.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
-							download.getTargetName());
-					if (to.exists()) {
-						notify(context, context.getString(R.string.application_name), context.getString(
-								R.string.msg_one_book_downloaded),
-								getIntent(context, to));
-					}
+						@Override
+						protected void onPostExecute(Bitmap image) {
+							super.onPostExecute(image);
+							File to = new File(App.Instance.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+									mDownload.getTargetName());
+							if (to.exists()) {
+								PendingIntent pi = getIntent(App.Instance, to);
+								if(image != null) {
+									notifyDownloadCompleted(App.Instance, System.currentTimeMillis(),
+											App.Instance.getString(R.string.application_name), App.Instance.getString(
+											R.string.msg_one_book_downloaded), image, pi);
+								} else {
+									fallbackNotify(
+											App.Instance,
+											System.currentTimeMillis(),
+											App.Instance.getString( R.string.application_name),
+											App.Instance.getString(R.string.msg_one_book_downloaded),
+											pi
+									);
+								}
+							}
+						}
+					}, download);
+
 
 					break;
 				case DownloadManager.STATUS_FAILED:
@@ -61,18 +100,6 @@ public final class DownloadReceiver extends BroadcastReceiver {
 			}
 		}
 		EventBus.getDefault().post(new DownloadCompleteEvent(download));
-	}
-
-
-	private static void notify(Context cxt, String title, String desc, PendingIntent contentIntent) {
-		NotificationManager mgr = (NotificationManager) cxt.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(cxt).setWhen(System.currentTimeMillis())
-				.setSmallIcon(
-				R.drawable.ic_download).setTicker(title).setContentTitle(title).setContentText(desc).setStyle(
-				new BigTextStyle().bigText(desc).setBigContentTitle(title)).setAutoCancel(true);
-		builder.setContentIntent(contentIntent);
-		mgr.notify((int) System.currentTimeMillis(), builder.build());
 	}
 
 
@@ -87,9 +114,42 @@ public final class DownloadReceiver extends BroadcastReceiver {
 		} catch (Exception ex) {
 			//Download pdf-reader.
 			String pdfReader = "com.adobe.reader";
-			contentIntent = PendingIntent.getActivity(cxt, (int) System.currentTimeMillis(), new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + pdfReader)),
+			contentIntent = PendingIntent.getActivity(cxt, (int) System.currentTimeMillis(), new Intent(
+					Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + pdfReader)),
 					PendingIntent.FLAG_ONE_SHOT);
 		}
 		return contentIntent;
+	}
+
+
+	private static void notifyDownloadCompleted(Context cxt, long id, String title, String desc, Bitmap image,
+			PendingIntent contentIntent )  {
+		NotificationManager mgr = (NotificationManager) cxt.getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(cxt).setWhen(id).setSmallIcon(
+				R.drawable.ic_download).setTicker(title).setContentTitle(title).setContentText(desc).setStyle(
+				new BigPictureStyle().bigPicture(image).setBigContentTitle(title)).setAutoCancel(true).setLargeIcon(
+				image);
+		builder.setContentIntent(contentIntent);
+		ringWorks(cxt, builder);
+		mgr.notify(Utils.randInt(1, 9999), builder.build());
+	}
+
+	private static void ringWorks(Context cxt, Builder builder) {
+		AudioManager audioManager = (AudioManager) App.Instance.getSystemService(Context.AUDIO_SERVICE);
+		if (audioManager.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+			builder.setVibrate(new long[] { 1000, 1000, 1000, 1000 });
+			builder.setSound(Uri.parse(String.format("android.resource://%s/%s", cxt.getPackageName(), R.raw.signal)));
+		}
+		builder.setLights(ContextCompat.getColor(App.Instance, R.color.primary_color), 1000, 1000);
+	}
+
+	private void fallbackNotify(Context cxt, long id, String title, String desc, PendingIntent contentIntent) {
+		NotificationManager mgr = (NotificationManager) cxt.getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(cxt).setWhen(id).setSmallIcon(
+				R.drawable.ic_download).setTicker(title).setContentTitle(title).setContentText(desc).setStyle(
+				new BigTextStyle().bigText(desc).setBigContentTitle(title)).setAutoCancel(true);
+		builder.setContentIntent(contentIntent);
+		ringWorks(cxt, builder);
+		mgr.notify(Utils.randInt(1, 9999), builder.build());
 	}
 }
