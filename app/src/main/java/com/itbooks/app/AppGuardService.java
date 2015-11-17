a28be55db9739a032fedbfae67c77489d260970e
XinyueZhao <zhao@eosnewmedia.de>
2015-11-17 14:05:17 +0100

    Fix wrong intent for scheduled sync notify

--- app/src/main/java/com/itbooks/app/AppGuardService.java
+++ app/src/main/java/com/itbooks/app/AppGuardService.java
@@ -1,9 +1,7 @@
 package com.itbooks.app;
 
 
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.text.TextUtils;
 
 import com.chopping.utils.NetworkUtils;
@@ -11,7 +9,6 @@ import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.TaskParams;
 import com.itbooks.R;
-import com.itbooks.app.activities.BookDetailActivity;
 import com.itbooks.net.SyncService;
 import com.itbooks.utils.NotifyUtils;
 import com.itbooks.utils.Prefs;
@@ -39,11 +36,8 @@ public final class AppGuardService extends GcmTaskService {
 	}
 
 	private static void notifySync(Context cxt) {
-		Intent intent = new Intent(cxt, BookDetailActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent contentIntent = PendingIntent.getActivity(cxt, com.chopping.utils.Utils.randInt(1, 9999), intent,
-				PendingIntent.FLAG_ONE_SHOT);
 		NotifyUtils.notifyWithoutBigImage(cxt, NOTIFY_ID, cxt.getString(R.string.application_name), cxt.getString(
-				R.string.msg_scheduled_sync_done), android.R.drawable.stat_notify_sync, contentIntent);
+				R.string.msg_scheduled_sync_done), android.R.drawable.stat_notify_sync,
+				com.itbooks.utils.NotifyUtils.getAppHome(cxt));
 	}
 }
